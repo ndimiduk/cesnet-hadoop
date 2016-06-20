66f82674109031ce8389240f68566afcc8bc0f63
Nick Dimiduk <ndimiduk@gmail.com>
2016-06-20 12:39:06 -0700

    add init script for journalnode

--- manifests/journalnode/service.pp
+++ manifests/journalnode/service.pp
@@ -3,6 +3,16 @@
 # Start Hadoop Journal Node daemon. See also hadoop::journalnode.
 #
 class hadoop::journalnode::service {
+
+  # HDP packages don't provide service scripts o.O
+  file {'/etc/init/hadoop-hdfs-journalnode.conf':
+    ensure  => file,
+    content => template('hadoop/services/hadoop-hdfs-journalnode.conf.erb'),
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+  }
+
   service { $hadoop::daemons['journalnode']:
     ensure    => 'running',
     enable    => true,
